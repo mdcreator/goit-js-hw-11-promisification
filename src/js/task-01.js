476b3aa7c617ebe2f9bcbe4aadde2c5dc3b022e6
Marina Domashenko <creator.marina@gmail.com>
2020-11-04 01:21:48 +0200

    Add tasks

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,8 +1,14 @@
+// Напиши функцию`delay(ms)`, которая возвращает промис, переходящий в состояние
+//     `"resolved"` через`ms` миллисекунд.Значением исполнившегося промиса должно
+// быть то кол - во миллисекунд которое передали во время вызова функции`delay`.
+
+// ```js
+
 const delay = ms => {
   // Твой код
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time} ms`);
 
 // Вызовы функции для проверки
 delay(2000).then(logger); // Resolved after 2000ms
