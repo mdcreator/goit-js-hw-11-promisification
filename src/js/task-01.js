0b6d401dd421b1e1725db2f4d6890aa5f5d89e7e
Marina Domashenko <creator.marina@gmail.com>
2020-11-04 23:08:37 +0200

    Add task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -6,6 +6,11 @@
 
 const delay = ms => {
   // Твой код
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time} ms`);
